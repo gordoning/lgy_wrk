4b2807bbdc331550d91b16b118a0cf4748f686c1
gordoning <linguoyang2008@126.com>
2017-09-27 02:14:50 +0800

    从表格中获取的字段，转换为整形

--- operate_excel.py
+++ operate_excel.py
@@ -9,17 +9,17 @@ nrows = table.nrows
 
 ncols = table.ncols
 
-for i in range(nrows):
-      print(table.row_values(i))
+for i in range(3,nrows):
+      print(int(table.row_values(i)[1]))
 
 
 wbk = xlwt.Workbook()
 
 sheet = wbk.add_sheet('我想要的表格')
 
-sheet.write(1, 1, '13616187656')  # 第0行第一列写入内容
-sheet.write(1, 2, '发券数量')  # 第0行第一列写入内容
-wbk.save('test.xls')
+# sheet.write(1, 1, '13616187656')  # 第0行第一列写入内容
+# sheet.write(1, 2, '发券数量')  # 第0行第一列写入内容
+# wbk.save('test.xls')
 
 with open('lgy.txt','a') as f:
     f.write('13616187656')
