20c71190ea54cc29ae8909ad2beaeab7e7bf17e7
gordoning <linguoyang2008@126.com>
2017-09-27 02:02:09 +0800

    增加操作excel文件的内容

---
+++ operate_excel.py
@@ -0,0 +1,27 @@
+import xlrd
+import xlwt
+
+workbook = xlrd.open_workbook('abc.xls')
+
+table = workbook.sheets()[0]
+
+nrows = table.nrows
+
+ncols = table.ncols
+
+for i in range(nrows):
+      print(table.row_values(i))
+
+
+wbk = xlwt.Workbook()
+
+sheet = wbk.add_sheet('我想要的表格')
+
+sheet.write(1, 1, '13616187656')  # 第0行第一列写入内容
+sheet.write(1, 2, '发券数量')  # 第0行第一列写入内容
+wbk.save('test.xls')
+
+with open('lgy.txt','a') as f:
+    f.write('13616187656')
+    f.write(',')
+    f.flush()
--- zhengshi_api.py
+++ zhengshi_api.py
@@ -1,10 +1,15 @@
 import requests,json
-
+from requests import HTTPError,Timeout
+import time
+import logging
 '''
 正式环境下的
 '''
 
 if __name__ == '__main__':
+
+    # 设置log文件
+    logging.basicConfig(filename='lgy_log.log', filemode="w", level=logging.INFO)
 
     # 用户端APP的头文件
 
@@ -30,20 +35,44 @@ if __name__ == '__main__':
 
     # 加盟商发送优惠券
     send_coupous_url = 'https://u.zhinengxiyifang.cn/api/Franchisees/6/sendCoupon'
-    send_coupous_params = {"serviceSubjectId": 6, "value": "0.01", "expireAt": "2020-09-08", "phones": [18579106120]}
-    # 17621778922,15961828736,17751522033,17770049131
 
-
+    # 手机号码列表
+    # phones = [15637238987,17635456545,13616187656]
+    phones_num = [{'phone':13616187656,'num':1},{'phone':13516352634,'num':3},{'phone':13616187656,'num':1}]
 
     #发起url的请求
     # resp_get = requests.get(url=getstore_device_url,headers=franchisees_header)
-    resp_post = requests.post(url=send_coupous_url,data=json.dumps(send_coupous_params),headers=franchisees_header)
+    # resp_post = requests.post(url=send_coupous_url,data=json.dumps(send_coupous_params),headers=franchisees_header)
 
-    #发多张券
-    for i in range(1,9):
-        resp_post = requests.post(url=send_coupous_url, data=json.dumps(send_coupous_params),
-                                  headers=franchisees_header)
+    # # resp_post = None
+    # print(phones_num[0]['phone'])
+    # 发多张券
+    for i in range(0,len(phones_num)):
+
+        phone = phones_num[i]['phone']
+        num = int(phones_num[i]['num'])
+        for j in range(0,num):
+
+            send_coupous_params = {"serviceSubjectId": 6, "value": "0.01", "expireAt": "2020-09-08",
+                                   "phones": [phone]}
+            try:
+                resp_post = requests.post(url=send_coupous_url, data=json.dumps(send_coupous_params),
+                                          headers=franchisees_header, timeout=3)
+                # print(phone,resp_post.status_code,"第{0}次".format(j+1))
+            except Timeout:
+                print("!!异常号码:{0}，已成功次数:{1}".format(phone,j))
+                logging.debug('This message should go to the log file')
+                logging.info("!!!异常号码:{0}，已发券:{1}".format(phone,j))
+                logging.warning('And this, too')
+                break
+
+        time.sleep(0.5)
+
+        print("已操作号码:{0}".format(phone))
+        logging.info("已操作号码:{0}".format(phone))
+
 
     #打印测试结果
-    print(resp_post.content.decode('utf-8'))
+    # print(resp_post.content.decode('utf-8'))
+    # print(resp_post.status_code)
 
